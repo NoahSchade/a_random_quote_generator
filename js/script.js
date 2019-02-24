2ecc2d9311d5d0e24302454840bf9eb89c66c680
Noah Schade <noah9@mail.com>
2019-02-24 16:28:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,43 +17,43 @@ project 1 - A Random Quote Generator
 
 const quotes = [
   {
-    "quote": "“Remember, too, that all who succeed in life get off to a bad start, and pass through many heartbreaking struggles before they \"arrive\". The turning point in the lives of those who succeed usually comes at some moment of crisis,through which they are introduced to their \"other selves\".”",
+    "quote": "Remember, too, that all who succeed in life get off to a bad start, and pass through many heartbreaking struggles before they \"arrive\". The turning point in the lives of those who succeed usually comes at some moment of crisis,through which they are introduced to their \"other selves\".",
     "source": "Napoleon Hill",
     "citation": "Book, Think and Grow Rich",
     "year": "1937",
     "tags": "Success/Failure"
   },
   {
-    "quote": "“One who has loved truly, can never lose entirely. Love is whimsical and temperamental. Its nature is ephemeral, and transitory. It comes when it pleases, and goes away without warning. Accept and enjoy it while it remains, but spend no time worrying about its departure. Worry will never bring it back.”",
+    "quote": "One who has loved truly, can never lose entirely. Love is whimsical and temperamental. Its nature is ephemeral, and transitory. It comes when it pleases, and goes away without warning. Accept and enjoy it while it remains, but spend no time worrying about its departure. Worry will never bring it back.",
     "source": "Napoleon Hill",
     "citation": "Book, Think and Grow Rich",
     "year": "1937",
     "tags": "Love"
   },
   {
-    "quote": "“Every adversity, every failure, every heartbreak, carries with it the seed of an equal or greater benefit.”",
+    "quote": "Every adversity, every failure, every heartbreak, carries with it the seed of an equal or greater benefit.",
     "source": "Napoleon Hill",
     "citation": "Book, Think and Grow Rich",
     "year": "1937",
     "tags": "Adversity, Success/Failure"
   },
   {
-    "quote": "“Failure is simply the opportunity to begin again, this time more intelligently.”",
+    "quote": "Failure is simply the opportunity to begin again, this time more intelligently.",
     "source": "Henry Ford",
     "tags": "Success/Failure"
   },
   {
-    "quote": "“When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.”",
+    "quote": "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
     "source": "Henry Ford",
     "tags": "Adversity"
   },
   {
-    "quote": "“Success is stumbling from failure to failure with no loss of enthusiasm”",
+    "quote": "Success is stumbling from failure to failure with no loss of enthusiasm",
     "source": "Winston Churchill",
     "tags": "Success/Failure"
   },
   {
-    "quote": "“Anyone who has never made a mistake has never tried anything new.”",
+    "quote": "Anyone who has never made a mistake has never tried anything new.",
     "source": "Albert Einstein",
     "tags": "Adversity"
   },
@@ -73,14 +73,13 @@ function getRandomQuote(quotes){
   return quotes[randomNumber];
 }
 
-console.log(getRandomQuote(quotes));
 
 /***
   Create the `printQuote` function to: 
    - Call the `getRandomQuote` function and assign it to a variable.
    - Create a variable for the HTML string and set it equal to an empty string.
    - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
+     the random quote variable to build your HTML string.
    - Add the quote and source section to the HTML string.
    - Use an if statement to check for the citation property before adding it to the HTML string.
    - Use an if statement to check for the year property before adding it to the HTML string.
@@ -90,9 +89,19 @@ console.log(getRandomQuote(quotes));
 
 function printQuote(){
   let randomQuote = getRandomQuote(quotes);
-  let html = "";
-}
+  let html = `<p class="quote"> ${randomQuote['quote']} </p>
+              <p class="source"> ${randomQuote['source']}`;
 
+              if (randomQuote['citation']) {
+               html += `<span class="citation"> ${randomQuote['citation']} </span>`;
+              }
+              if (randomQuote['year']) {
+               html += `<span class="year"> ${randomQuote['year']} </span>`;
+              }
+              html += "</p>";
+              document.getElementById("quote-box").innerHTML = html;
+  return html
+}
 
 
 /***
